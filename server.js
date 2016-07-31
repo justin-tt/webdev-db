97977635d1e7f19a1b13247df938f4026080d93c
justin-tt <justin.ch.goh+tt@gmail.com>
2016-08-01 05:51:43 +0800

    create joke functionality

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ var ObjectId = require('mongodb').ObjectID;
 var app = express();
 var port = 8888;
 
+// setting ENV variables in windows
+// https://technet.microsoft.com/en-us/library/ff730964.aspx
+// [Environment]::SetEnvironmentVariable("TestVariable", "Test value.", "User")
+// requires restarting of powershell to view variables
+
 var uri = "mongodb://" + process.env.MONGOLAB_USER + ":" + process.env.MONGOLAB_PASSWORD + "@ds023485.mlab.com:23485/webdev";
-// var jokes=[{setup:"Our wedding was so beautiful,",punchline:"even the cake was in tiers", votes: 0},{setup:"I'm reading a book on the history of glue",punchline:"I just can't seem to put it down", votes: 0},{setup:"What do you call an Argentinian with a rubber toe?",punchline:"Roberto", votes: 0}];
+var jokes=[{setup:"Our wedding was so beautiful,",punchline:"even the cake was in tiers", votes: 0},{setup:"I'm reading a book on the history of glue",punchline:"I just can't seem to put it down", votes: 0},{setup:"What do you call an Argentinian with a rubber toe?",punchline:"Roberto", votes: 0}];
 
 app.use(bodyParser.json());
 app.use(express.static('static'));
@@ -95,16 +100,63 @@ app.post('/downvote', function(req, res) {
     });
 });
 
-app.get('/admin', function(req, res) {
+/* This page was to populate some jokes into the database */
+// app.get('/admin', function(req, res) {
+//
+//     console.log('Attempting to add into database.');
+//     mongodb.connect(uri, function(err, db) {
+//         var collection = db.collection('jokes');
+//         collection.insertMany(jokes,
+//             function(err, results){
+//                 res.send(results);
+//                 db.close();
+//             }
+//         );
+//     });
+// });
 
-    console.log('Attempting to add into database.');
+app.get('/updatejokes', function(req, res) {
+
+    res.sendFile(__dirname + '/static/updateJokes.html');
+});
+
+app.post('/createJoke', function(req, res) {
+
+    console.log('Creating joke: ', req.body);
+
     mongodb.connect(uri, function(err, db) {
         var collection = db.collection('jokes');
-        collection.insertMany(jokes,
+        collection.insertOne(req.body,
             function(err, results){
                 res.send(results);
                 db.close();
             }
         );
     });
+
 });
+
+app.get('/alljokes', function(req, res) {
+    var jokesArray;
+    mongodb.connect(uri, function(err, db) {
+        var collection = db.collection('jokes');
+        // http://blog.modulus.io/mongodb-tutorial
+        collection.find().toArray(
+            function(err, result) {
+                jokesArray = result;
+                res.send(jokesArray)
+            }
+        );
+    });
+});
+
+app.post('/deletejoke', function(req, res) {
+    console.log("Deleting: " + req.body.id);
+    // TODO: actually delete it from the database
+});
+
+// TODO: READ/UPDATE/DELETE Jokes
+// Have a drop down list of jokes (READ)
+// this dropdown should have a function that re-loads db documents when jokes are added/deleted/edited
+// to edit (UPDATE)
+// or delete (DELETE)
--- static/app.js
+++ static/app.js
@@ -57,6 +57,63 @@ $(function() {
         });
     });
 
+    // should have it's own file for the database page
+    $("#createJoke").submit(function(event) {
+        event.preventDefault();
+
+        var setup = $('#createJokeSetup').val();
+        var punchline = $('#createJokePunchline').val();
+
+        // https://www.airpair.com/js/jquery-ajax-post-tutorial
+        $.ajax({
+            url: '/createJoke',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(
+                {
+                    "setup": setup,
+                    "punchline": punchline,
+                    "votes": 0
+                }
+            ),
+            success: function(data, status, xhr) {
+                $('#createJokeSetup').val("");
+                $('#createJokePunchline').val("");
+                $('#createJokeSuccess').text("Joke created successfully. {setup: " + setup + ", punchline: " + punchline + "}");
+            }
+        });
+    });
+
+    $("#refreshJokes").on('click', function() {
+        $.get("/alljokes", function(data) {
+            console.log(data);
+            var optionsHTML = "";
+            $('#allJokes').empty();
+            data.forEach(function(element, index, array) {
+                console.log(element, index, array);
+                // http://stackoverflow.com/questions/1801499/how-to-change-options-of-select-with-jquery
+                $('#allJokes').append($("<option></option>").attr("value",element._id).text(element.setup));
+            });
+        });
+    });
+
+    $("#deleteJoke").on('click', function() {
+       $.ajax({
+           url: 'deletejoke',
+           type: 'POST',
+           contentType: 'application/json',
+           data: JSON.stringify(
+               {
+                    id: $('#allJokes').val()
+               }
+           ),
+            success: function(data, status, xhr) {
+               // TODO: refresh menu
+                console.log($('#allJokes').val());
+            }
+       })
+    });
+
     function changeVoteColor(numVotes) {
         if (numVotes < 0) {
             $("#votes").css('color', 'red');
