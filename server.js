ffd8f90d92059cbcd19d2e76ac0443893ac8dac2
justin-tt <justin.ch.goh+tt@gmail.com>
2016-08-10 20:38:49 +0800

    refactored with connection pooling

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongodb = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var app = express();
 var port = 8888;
+var db, collection;
 
 // setting ENV variables in windows
 // https://technet.microsoft.com/en-us/library/ff730964.aspx
@@ -16,9 +17,15 @@ var jokes=[{setup:"Our wedding was so beautiful,",punchline:"even the cake was i
 app.use(bodyParser.json());
 app.use(express.static('static'));
 
-app.listen(process.env.PORT || port, function(){
-    console.log("Listening on " + port);
+mongodb.connect(uri, function(err, database) {
+   db = database;
+   collection = db.collection('jokes');
+
+    app.listen(process.env.PORT || port, function() {
+        console.log("Listening on " + port);
+    });
 });
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/static/index.html');
@@ -26,18 +33,16 @@ app.get('/', function(req, res) {
 
 app.get('/jokes', function(req, res) {
 
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.count(function(err, count) {
-            var randomNumber = Math.floor(Math.random() * count);
-            console.log(count + " jokes found in database.");
+    collection.count(function(err, count) {
 
-            collection.find().limit(-1).skip(randomNumber).next(
-                function(err, results) {
-                    res.send(results);
-                }
-            );
-        });
+        var randomNumber = Math.floor(Math.random() * count);
+        console.log(count + " jokes found in database.");
+
+        collection.find().limit(-1).skip(randomNumber).next(
+            function(err, results) {
+                res.send(results);
+            }
+        );
     });
 });
 
@@ -45,42 +50,33 @@ app.post('/upvote', function(req, res) {
     console.log("Someone tried to upvote something");
     console.log(req.body);
     var jokeIndex = req.body.id;
-
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        // https://docs.mongodb.com/manual/reference/method/db.collection.findOneAndUpdate/
-        collection.findOneAndUpdate(
-            { _id: ObjectId(jokeIndex) },
-            { $inc: { "votes" : 1 }},
-            { returnNewDocument: true },
-            function(err, result) {
-                result.value.votes++;
-                console.log(result);
-                res.send(result.value);
-            }
-        );
-    });
+    // https://docs.mongodb.com/manual/reference/method/db.collection.findOneAndUpdate/
+    collection.findOneAndUpdate(
+        { _id: ObjectId(jokeIndex) },
+        { $inc: { "votes" : 1 }},
+        function(err, result) {
+            result.value.votes++;
+            console.log(result);
+            res.send(result.value);
+        }
+    );
 });
 
 app.post('/downvote', function(req, res) {
     console.log("Someone tried to downvote something.");
     console.log(req.body);
     var jokeIndex = req.body.id;
-
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        // https://docs.mongodb.com/manual/reference/operator/update/  INCREMENT
-        collection.findOneAndUpdate(
-            { _id: ObjectId(jokeIndex) },
-            { $inc: { "votes" : -1 }},
-            { returnNewDocument: true },
-            function(err, result) {
-                result.value.votes--;
-                console.log(result);
-                res.send(result.value);
-            }
-        );
-    });
+    // https://docs.mongodb.com/manual/reference/operator/update/  INCREMENT
+    collection.findOneAndUpdate(
+        { _id: ObjectId(jokeIndex) },
+        { $inc: { "votes" : -1 }},
+        { returnNewDocument: true },
+        function(err, result) {
+            result.value.votes--;
+            console.log(result);
+            res.send(result.value);
+        }
+    );
 });
 
 /* This page was to populate some jokes into the database */
@@ -107,47 +103,29 @@ app.post('/createJoke', function(req, res) {
 
     console.log('Creating joke: ', req.body);
 
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.insertOne(req.body,
-            function(err, results){
-                res.send(results);
-                db.close();
-            }
-        );
-    });
-
+    collection.insertOne(req.body,
+        function(err, results){
+            res.send(results);
+        }
+    );
 });
 
 app.get('/alljokes', function(req, res) {
     var jokesArray;
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        // http://blog.modulus.io/mongodb-tutorial
-        collection.find().toArray(
-            function(err, result) {
-                jokesArray = result;
-                res.send(jokesArray)
-            }
-        );
-    });
+    // http://blog.modulus.io/mongodb-tutorial
+    collection.find().toArray(
+        function(err, result) {
+            jokesArray = result;
+            res.send(jokesArray)
+        }
+    );
 });
 
 app.post('/deletejoke', function(req, res) {
     console.log("Deleting: " + req.body.id);
-
-    mongodb.connect(uri, function(err, db) {
-        var collection = db.collection('jokes');
-        collection.deleteOne({_id: ObjectId(req.body.id)},
-            function(err, results){
-                res.send(results);
-                db.close();
-            }
-        );
-    });
+    collection.deleteOne({_id: ObjectId(req.body.id)},
+        function(err, results){
+            res.send(results);
+        }
+    );
 });
-
-// TODO: UPDATE Jokes
-// Have a drop down list of jokes (READ)
-// this dropdown should have a function that re-loads db documents when jokes are added/deleted/edited
-// to edit (UPDATE)
